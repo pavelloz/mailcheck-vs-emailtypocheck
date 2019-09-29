1b6032bd0a99fda6559fa607fe730310f2dbc48c
Paweł Kowalski <pavelloz@gmail.com>
2019-09-29 21:14:46 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import Mailcheck from 'mailcheck';
+import { checkEmailTypos } from 'emailtypocheck/dist/emailtypocheck.js';
+
+const input = document.querySelector('[data-input]');
+const resultMailcheck = document.querySelector('[data-result="mailcheck"]');
+const resultEmailtypocheck = document.querySelector('[data-result="emailtypocheck"]');
+
+const runMailcheck = email => {
+  Mailcheck.run({
+    email,
+    suggested: suggestion => {
+      resultMailcheck.textContent = suggestion.full;
+    }
+  });
+};
+
+const runEmailtypocheck = email => {
+  try {
+    resultEmailtypocheck.textContent = checkEmailTypos(email).join(', ');
+  } catch(e) {};
+};
+
+input.addEventListener('keyup', e => {
+  const email = event.target.value || '';
+
+  runMailcheck(email);
+  runEmailtypocheck(email);
+});
+
+const ev = new CustomEvent('keyup');
+input.dispatchEvent(ev);
